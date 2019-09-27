c8af3a6454759b85ff4500c16b72f59edcdf6bf5
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-27 05:47:43 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,4 @@
-class CreateArtists < ActiveRecord::Migration[4.2]
+class CreateArtists < ActiveRecord::Migration[5.2]
   def change
     create_table :artists do |t|
       t.string :name
@@ -6,5 +6,10 @@ class CreateArtists < ActiveRecord::Migration[4.2]
       t.integer :age
       t.string :hometown
     end
+  end
+  def up
+  end
+
+  def down
   end
 end
